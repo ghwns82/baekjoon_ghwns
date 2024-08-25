495eb71c681d8835f28ff9ae8c56636116bb2216
ghwns82 <68892482+ghwns82@users.noreply.github.com>
2024-08-25 21:39:54 +0900

    [Bronze V] Title: 시험 성적, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int score = sc.nextInt();
+		String result = "FFFFFFDCBAA";
+		score=score/10;
+//		System.out.println(score);
+		System.out.println(result.charAt(score));
+		
+		
+	}
+}
