53552ca6e1b098edce2dacdc7f04f11ac632b863
ghwns82 <68892482+ghwns82@users.noreply.github.com>
2024-09-28 23:55:44 +0900

    [Platinum III] Title: 어려운 하노이 탑, Time: 36 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/27743. 어려운 하노이 탑/어려운 하노이 탑.py
+++ 백준/Platinum/27743. 어려운 하노이 탑/어려운 하노이 탑.py
@@ -1,22 +1,11 @@
 n,m = map(int,input().split())
 mod = 10**9 + 7
 if m == 1:
-    print(pow(2,n,mod)-1)
+    print((pow(2,n,mod)-1)%mod)
 
 else:
-    result = 1
-    result *= 4*m
+    result = 2*m-1
     result %=mod
-    if n%2:
-        result *= (n-1)//2
-        result %=mod
-        result *= n
-        result %= mod
-    else:
-        result *= n//2
-        result %=mod
-        result *= (n-1)
-        result %= mod
-    result += 2*m-1
+    result += 4*m* (pow(2,n-1,mod)-1)
     result %=mod
     print(result)
