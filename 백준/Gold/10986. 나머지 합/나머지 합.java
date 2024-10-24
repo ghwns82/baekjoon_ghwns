e81fc826a2828be99f9f6f7bf5e1c24b0ce1c943
ghwns82 <68892482+ghwns82@users.noreply.github.com>
2024-10-24 20:27:49 +0900

    [Gold III] Title: 나머지 합, Time: 412 ms, Memory: 160364 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,39 +1,46 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        // BufferedReader로 입력받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        // 입력 받기
-        int N = sc.nextInt(); // 배열의 크기
-        int M = sc.nextInt(); // 나눌 값
-        long[] A = new long[N + 1]; // 부분합을 저장할 배열
+        // 첫 번째 줄에서 N과 M 입력 받기
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         
-        // 입력 받은 배열 요소의 누적 합 구하기
-        for (int i = 1; i <= N; i++) {
-            A[i] = A[i - 1] + sc.nextInt();
-        }
+        long[] mod = new long[M]; // 나머지 카운트를 저장할 배열
         
-        // 나머지 카운트를 저장할 배열 (0 ~ M-1 까지의 값)
-        long[] mod = new long[M];
-        
+        long sum = 0;
         long result = 0;
-        
-        // 누적합의 나머지 계산
-        for (int i = 1; i <= N; i++) {
-            int remainder = (int) (A[i] % M);
+
+        // 배열 요소를 입력 받으며 나머지 계산과 카운팅 수행
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            sum += num;
+            
+            // sum이 M보다 크면 M으로 나머지 연산 수행
+            int remainder = (int)(sum % M);
             if (remainder < 0) remainder += M; // 음수 나머지 방지
             
-            // 나머지가 0인 경우는 그 자체로 나누어 떨어지는 구간
-            if (remainder == 0) result++;
-            
-            // 이전에 같은 나머지가 나온 횟수를 더함
-            result += mod[remainder];
-            
-            // 현재 나머지 카운트를 1 증가
-            mod[remainder]++;
+            mod[remainder]++; // 나머지 값에 해당하는 카운트를 증가
         }
-        
+
+        // 나머지가 0인 경우 그 자체로 나누어 떨어지는 구간
+        result += mod[0];
+
+        // 나머지가 같은 것들로 만들 수 있는 구간의 개수 계산
+        for (int i = 0; i < M; i++) {
+            if (mod[i] > 1) {
+                result += mod[i] * (mod[i] - 1) / 2;  // 조합을 이용하여 구간의 개수 계산
+            }
+        }
+
         // 결과 출력
         System.out.println(result);
     }
