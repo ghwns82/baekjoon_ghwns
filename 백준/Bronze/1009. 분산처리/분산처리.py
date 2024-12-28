bbfc863331b357916a368c84c2f7b1b7800d3800
ghwns82 <68892482+ghwns82@users.noreply.github.com>
2024-12-28 10:49:33 +0900

    [Bronze II] Title: 분산처리, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -3,10 +3,7 @@ input = lambda : sys.stdin.readline().strip()
 
 for i in range(int(input())):
     a,b = map(int,input().split())
-    c = 1
-    for i in range(b):
-        c *=a
-        c %=10
+    c = pow(a,b,10)
     if not c:
         c+=10
     print(c)
