b83e9d24051c91cef14ad0715a73f43975a71045
ghwns82 <68892482+ghwns82@users.noreply.github.com>
2024-12-28 12:08:44 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,21 +1,17 @@
 N,M = map(int, input().split())
 chess = [input() for _ in range(N)]
-count_list = []
+result = 64
 
 
 for x in range(N-7):
     for y in range(M-7):
-        idx1 = 0
-        idx2 = 0
+        cnt = 0
         for b in range(x, x + 8):
             for j in range(y, y + 8):              
                 if (j + b)%2 == 0:
-                    if chess[b][j] != 'W': idx1 += 1  
-                    if chess[b][j] != 'B': idx2 += 1
+                    if chess[b][j] != 'W': cnt += 1  
                 else :
-                    if chess[b][j] != 'B': idx1 += 1
-                    if chess[b][j] != 'W': idx2 += 1
-        count_list.append(idx1)                          
-        count_list.append(idx2)                          
-
-print(min(count_list))               
+                    if chess[b][j] != 'B': cnt += 1
+        cnt = min(cnt, 64-cnt)
+        result = min (result, cnt)
+print(result)
