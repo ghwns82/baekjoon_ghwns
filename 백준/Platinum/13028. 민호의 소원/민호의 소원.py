e6717cbac06e17e94d99824c3dab1374c14c09c0
ghwns82 <68892482+ghwns82@users.noreply.github.com>
2025-02-02 18:47:53 +0900

    [Platinum II] Title: 민호의 소원, Time: 2964 ms, Memory: 187164 KB -BaekjoonHub

---
+++ 백준/Platinum/13028. 민호의 소원/민호의 소원.py
@@ -0,0 +1,47 @@
+import sys
+from collections import Counter
+input = lambda: sys.stdin.readline().strip()
+n, q = map(int,input().split())
+arr =list(map(int,input().split()))
+sq = n**0.5
+queries = sorted([list(map(int,input().split()))+[i,0] for i in range(q)], key=lambda x: (x[0]//sq,x[1]) )
+ct = Counter()
+table = [0]*100_001
+ct[arr[0]]+=1
+table[1]=1
+left,right=1,1
+res = 0
+def plus(v,res):
+    table[ct[v]]-=1
+    ct[v]+=1
+    if ct[v]==3:
+        res+=1
+    table[ct[v]]+=1
+    return res
+    
+def minus(v,res):
+    table[ct[v]]-=1
+    if ct[v]==3:
+        res-=1
+    ct[v]-=1
+    table[ct[v]]+=1
+    return res
+
+for query in queries:
+    while left < query[0]:
+        res=minus(arr[left-1],res)
+        left+=1
+    while left > query[0]:
+        left-=1
+        res=plus(arr[left-1],res)
+        
+    while right < query[1]:
+        right+=1
+        res=plus(arr[right-1],res)
+    while right > query[1]:
+        res = minus(arr[right-1],res)
+        right-=1
+    query[3]=res
+
+for query in sorted(queries, key=lambda x: x[2]):
+    print(query[3])
