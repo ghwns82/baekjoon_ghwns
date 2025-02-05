4a0df3fa9a730e0d03ae7fb8940c08769831142b
ghwns82 <68892482+ghwns82@users.noreply.github.com>
2025-02-06 03:38:24 +0900

    [Platinum III] Title: 수열과 쿼리 1, Time: 1268 ms, Memory: 188376 KB -BaekjoonHub

---
+++ 백준/Platinum/13537. 수열과 쿼리 1/수열과 쿼리 1.py
@@ -0,0 +1,45 @@
+import sys
+from bisect import bisect_right
+input = lambda: sys.stdin.readline().strip()
+n=int(input())
+arr = list(map(int,input().split()))
+m = int(input())
+queries = [list(map(int,input().split())) for i in range(m)]
+
+class Node:
+    def __init__(self,start,end,left=None,right=None,list=None):
+        self.range = (start,end)
+        if list:
+            self.data = list
+        else:
+            self.data = sorted(left.data+right.data)
+        self.left=left
+        self.right=right
+
+class MS:
+    def __init__(self):
+        self.root=self.create(0,n-1)
+    def create(self, start,end):
+        if start==end:
+            return Node(start,end,list=[arr[start]])
+        mid = (start+end)//2
+        return Node(start,end,self.create(start,mid),self.create(mid+1,end))
+    def search(self, nd:Node,s,e,k):
+        
+        start,end =nd.range
+        if start==s and end ==e:
+            return len(nd.data)-bisect_right(nd.data, k)
+        
+        mid = (start+end)//2
+        # print(nd.range,mid,[s,e])
+        if mid<s:
+            return self.search(nd.right, s,e,k)
+        elif e<=mid:
+            return self.search(nd.left,s,e,k)
+        else:
+            return self.search(nd.left, s,mid,k) + self.search(nd.right,mid+1,e,k)
+        
+    
+mstree = MS()
+for i,j,k in queries:
+    print(mstree.search(mstree.root,i-1,j-1,k))
