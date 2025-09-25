d150523a42e99c2f68d5a2138a89c4557a1faa42
ghwns82 <68892482+ghwns82@users.noreply.github.com>
2025-09-25 17:19:14 +0900

    [Silver II] Title: 정보대 등산, Time: 340 ms, Memory: 53352 KB -BaekjoonHub

--- 백준/Silver/12841. 정보대 등산/정보대 등산.java
+++ 백준/Silver/12841. 정보대 등산/정보대 등산.java
@@ -8,44 +8,45 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
-        long[] cross = new long[n];
+        int[] cross = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            cross[i] = Long.parseLong(st.nextToken());
+            cross[i] = Integer.parseInt(st.nextToken());
         }
 
-        long[] left = new long[n - 1];
+        int[] left = new int[n - 1];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n - 1; i++) {
-            left[i] = Long.parseLong(st.nextToken());
+            left[i] = Integer.parseInt(st.nextToken());
         }
 
-        long[] right = new long[n - 1];
+        int[] right = new int[n - 1];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n - 1; i++) {
-            right[i] = Long.parseLong(st.nextToken());
+            right[i] = Integer.parseInt(st.nextToken());
         }
 
         long[] nsLeft = new long[n];
         long[] nsRight = new long[n];
+        nsLeft[0] = 0;
+        nsRight[0] = 0;
+
         for (int i = 1; i < n; i++) {
             nsLeft[i] = nsLeft[i - 1] + left[i - 1];
             nsRight[i] = nsRight[i - 1] + right[i - 1];
         }
 
-        // 초기값 (파이썬 result = [~, -1])
-        long bestCost = nsLeft[n - 1] + nsRight[n - 1] + cross[n - 1];
-        int bestIndex = -1;
+        long minValue = nsLeft[n - 1] + nsRight[n - 1] + cross[n - 1];
+        int minIndex = -1;
 
         for (int i = 0; i < n; i++) {
             long tmp = nsLeft[i] + (nsRight[n - 1] - nsRight[i]) + cross[i];
-            // 파이썬의 min([cost, idx])와 동일하게 처리
-            if (tmp < bestCost || (tmp == bestCost && (i + 1) < bestIndex)) {
-                bestCost = tmp;
-                bestIndex = i + 1;
+            if (tmp < minValue) {
+                minValue = tmp;
+                minIndex = i + 1; // 출력은 1부터 시작
             }
         }
 
-        System.out.println(bestIndex + " " + bestCost);
+        System.out.println(minIndex + " " + minValue);
     }
 }
