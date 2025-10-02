c0202fbd73f1e58c0b7ccbb9e1225d1575587de5
ghwns82 <68892482+ghwns82@users.noreply.github.com>
2025-10-02 20:44:46 +0900

    [Platinum V] Title: 등산 마니아, Time: 752 ms, Memory: 237788 KB, Score: 47 point -BaekjoonHub

---
+++ 백준/Platinum/20188. 등산 마니아/등산 마니아.py
@@ -0,0 +1,28 @@
+import math
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+graph = [set() for i in range(n)]
+
+for i in range(n-1):
+    a,b = map(int,input().split())
+    graph[a-1].add(b-1)
+    graph[b-1].add(a-1)
+    
+def dfs(cur):
+    global n, result
+    dots = 1
+    for next in graph[cur]:
+        if not visited[next]:
+            visited[next] = 1
+            dots += dfs(next)
+    if cur!=0:
+        result+=math.comb(n,2)-math.comb(n-dots,2)
+    return dots
+
+visited = [0]*n
+result = 0
+visited[0] = 1
+dfs(0)
+print(result)
